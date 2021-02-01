0acbae591f520fc655a15bd3613a08d80d187571
ihab abadi <ihab@utopios.net>
2021-02-01 11:42:05 +0100

    version 1

---
+++ app/src/main/java/com/example/courscesiandroid/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.courscesiandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        ConstraintLayout c = new ConstraintLayout(this);
+        /*Ajout largeur et hauteru du layout*/
+        /*c.setMaxWidth();*/
+        /*TextView text = new TextView(this);
+        text.setText("Contenu du text");
+        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        c.addView(text, params);*/
+        TextView text = new TextView(this);
+        text.setText("Contenu Ajouté");
+        setContentView(R.layout.activity_main);
+        RelativeLayout layout = findViewById(R.id.main_activity_relative_layout);
+        layout.addView(text, RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        Button b1 = (Button) findViewById(R.id.b1);
+        /*b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button b = (Button) v;
+                b.setText(b.getText()+" Added after click");
+            }
+        });*/
+       /* b1.setOnClickListener((v) -> {
+            Button b = (Button) v;
+            b.setText(b.getText()+" Added after click");
+        });*/
+    }
+
+    public void onClickb1(View v) {
+        Button b = (Button) v;
+        b.setText(b.getText()+" Added after click");
+    }
+}
---
+++ app/src/main/java/com/example/courscesiandroid/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.courscesiandroid;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
